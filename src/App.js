778912b7ca597657dd23474c8b5ec4c94023fccf
Bevan Jacinto <bevanjacinto.bj@gmail.com>
2023-11-28 15:45:16 -0800

    Added basic styling and layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop"><img src={gptLogo} alt="" className="logo" /><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt="" />What is programming?</button>
+            <button className="query"><img src="" alt="" />What is programming?</button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
