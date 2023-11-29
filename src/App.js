7c570deffec900b9919bf630880434b2a0d73095
Bevan Jacinto <bevanjacinto.bj@gmail.com>
2023-11-28 16:14:17 -0800

    Added icons and padding/margin to upperside

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import "./App.css";
-import gptLogo from "./assets/chatgpt.svg"
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import rocket from './assets/rocket.svg';
+import saved from './assets/bookmark.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="sideBar">
         <div className="upperSide">
-          <div className="upperSideTop"><img src={gptLogo} alt="" className="logo" /><span className="brand">ChatGPT</span></div>
-          <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
+          <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo" /><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
           <div className="upperSideBottom">
-            <button className="query"><img src="" alt="" />What is programming?</button>
-            <button className="query"><img src="" alt="" />What is programming?</button>
+            <button className="query"><img src={msgIcon} alt="Query" />What is programming?</button>
+            <button className="query"><img src={msgIcon} alt="Query" />How to use an API</button>
           </div>
         </div>
-        <div className="lowerSide"></div>
+        <div className="lowerSide">
+          <div className="listItems"><img src={home} alt="Home" className="listItemsImg" />Home</div>
+          <div className="listItems"><img src={saved} alt="Saved" className="listItemsImg" />Saved</div>
+          <div className="listItems"><img src={rocket} alt="Upgrade" className="listItemsImg" />Upgrade to Pro</div>
+        </div>
       </div>
       <div className="main"></div>
     </div>
