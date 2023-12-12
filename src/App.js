875e9e1832a815575471f86660e14f37847b0a63
Bevan Jacinto <bevanjacinto.bj@gmail.com>
2023-12-11 22:04:13 -0800

    Added static components for the main section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import msgIcon from './assets/message.svg';
 import home from './assets/home.svg';
 import rocket from './assets/rocket.svg';
 import saved from './assets/bookmark.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
 
 function App() {
   return (
@@ -24,7 +27,18 @@ function App() {
           <div className="listItems"><img src={rocket} alt="Upgrade" className="listItemsImg" />Upgrade to Pro</div>
         </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat"><img className="chatimg" src={userIcon} alt="" /><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita consequuntur exercitationem a suscipit nisi earum quidem minus labore. Mollitia.</p></div>
+          <div className="chat bot"><img className="chatimg" src={gptImgLogo} alt="" /><p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero excepturi minima repudiandae necessitatibus laboriosam vel. Beatae dolor mollitia corporis, deserunt, expedita, voluptas at harum ipsam maiores quaerat vitae voluptatem perspiciatis aliquam. Ea dolore eveniet vero alias illo? Nemo modi tempora quam, totam, fugit ipsum unde animi repellendus fuga velit dicta ea optio pariatur explicabo ullam qui assumenda. Harum ullam quasi dicta ipsum eveniet repudiandae! Tempora illo inventore iusto voluptatibus officiis repellat ea at ipsam quisquam ex animi unde enim quaerat aspernatur totam similique, recusandae saepe nostrum vitae, rerum sed consectetur. Voluptas magni doloremque voluptate iusto! Voluptatum, praesentium rem. Omnis, consequuntur.</p></div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="Send a message"/><button className="send"><img src={sendBtn} alt="Send" /></button>
+          </div>
+          <p>ChatGPT may produce inaccurate information about people, places or facts</p>
+        </div>
+      </div>
     </div>
   );
 }
